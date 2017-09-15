1a2621049e62587d2cf7c243c877c22f7c0d2967
Rachit Bhasin <10251396@bd.com>
2017-09-15 19:26:57 +0530

    Added basic routing

--- app/app.js
+++ app/app.js
@@ -1,3 +1,23 @@
 'use strict'
 
-var app = angular.module('MCQ', ['ui.router']);
+var mcqApp = angular.module('MCQ', ['ui.router']);
+
+
+
+mcqApp.config(['$stateProvider', ($stateProvider) => {
+
+	var homeState = {
+		name: 'home',
+		url: '/home',
+		template: '<h3>Home</h3>'
+	}
+
+	var aboutState = {
+		name: 'about',
+		url: '/about',
+		template: '<h3>About</h3>'
+	}
+
+	$stateProvider.state(homeState);
+  	$stateProvider.state(aboutState);
+}])
